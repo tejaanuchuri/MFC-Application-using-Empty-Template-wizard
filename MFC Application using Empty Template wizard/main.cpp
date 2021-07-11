df451d0dc80aa06f69631959b3d311d2d415fc9c
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-07-12 02:13:26 +0530

    Add project files.

---
+++ MFC Application using Empty Template wizard/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+
+void main() {
+	cout << "***************************************\n";
+	cout << "MFC Application Using Empty Project Wizard...! ";
+	cout << "\n***************************************";
+	getchar();
+}
